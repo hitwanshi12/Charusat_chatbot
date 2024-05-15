a0ea915b86e8327c2b11da8bb4c8d98207a3bfcf
hitwanshi12 <121186168+hitwanshi12@users.noreply.github.com>
2024-05-15 10:19:43 +0530

    Add files via upload

---
+++ client/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Faqpage from './faqPage';
+import FaqCont from './faqcont';
+import Cont from './faqcont';
+
+function App() {
+  return (
+    <div className="App">
+      <FaqCont></FaqCont>
+    </div>
+  );
+}
+
+export default App;
